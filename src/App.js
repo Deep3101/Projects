cb5d82e913797a0d07a03959bb2a3d93ad2de513
Deep S Panchal <109277677+DeepSPanchal@users.noreply.github.com>
2023-01-24 23:34:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Temp from './component/weather/temp';
+import Restaurantapp from './component/basics/Restaurantapp';
+
+
+const App = () => {
+  return (
+    <Restaurantapp />
+  )
+}
+
+export default App;
---
+++ src/component/basics/MenuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggie.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I Really Love Maggie very much its too yummy,tasty and delicious thats why its called 'Meri Maggie' ",
+    },
+  
+    {
+      id: 2,
+      image: "images/Aloopakoda.jpg",
+      name: "aaloo pakora",
+      category: "evening",
+      price: "20₹",
+      description:
+        "No Words to describe about this amazing dish madeup of potatoes stuffed in it then fried yummn yummm!!!!",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description: 
+     "Corn is rich in vitamin C, an antioxidant that helps protect your cells from damage and wards off diseases like cancer and heart disease. "
+    },
+    {
+      id: 4,
+      image: "images/chole.jpg",
+      name: "Chole",
+      category: "Lunch",
+      price: "50₹",
+      description:
+      "Chole bhature (छोले भटूरे) is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a fried bread made from maida."
+   
+    },
+    {
+      id: 5,
+      image: "images/pizza.jpg",
+      name: "Pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+      "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese."
+    },
+    {
+      id: 6,
+      image: "images/vegthali.jpg",
+      name: "Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+      "Its a typical gujurati thali which contains various varieties of different food items such as roti,sabji,dal,rice and gulab jamun."
+    },
+    {
+      id: 7,
+      image: "images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+      "There are various sweet dishes such as kajukatri,rasgulla,gulab jamun,mohanthaal and many more..."
+    },
+    {
+      id: 8,
+      image:"images/rajma.jpg",
+      name: "Rajma Rice",
+      category: "Lunch",
+      price: "60₹",
+      description:
+      "These are smaller in size than most Rajma grown in plains and have a slightly sweetish taste. The combination of Rajma and rice generally lists as a top favorite of North Indians and Nepalis."
+    },
+    {
+      id: 9,
+      image: "images/samosa.jpg",
+      name: "Samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+      "A samosa is a fried or baked pastry with a savory filling, including ingredients such as spiced potatoes, onions, and peas."
+    },
+  ];
+  
+  export default Menu;
